16058c3287f84556e2fb9f19d2b3fdfc84f55496
sanha <kgpaper@naver.com>
2020-12-28 17:48:42 +0900

    1228 commit

--- cafein/src/main/java/DSC4/cafein/controller/SearchController.java
+++ cafein/src/main/java/DSC4/cafein/controller/SearchController.java
@@ -1,8 +1,10 @@
 package DSC4.cafein.controller;
 
+import DSC4.cafein.domain.Cafe;
 import DSC4.cafein.service.FirebaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +14,20 @@ public class SearchController {
     FirebaseService firebaseService;
 
     @GetMapping("/search")
-    public String search() {
+    public String search(Model model) throws Exception {
+
+        Cafe cafe = new Cafe();
+        cafe = firebaseService.getCafeDetail("cafe1");
+
+        String occupied_s = Integer.toString(cafe.getOccupied_s());
+        String total_s = Integer.toString(cafe.getTotal_s());
+
+        System.out.println(occupied_s);
+        System.out.println(total_s);
+
+
+        model.addAttribute("occupied_s", occupied_s);
+        model.addAttribute("total_s", total_s);
 
         return "search";
     }
--- cafein/src/main/java/DSC4/cafein/domain/Cafe.java
+++ cafein/src/main/java/DSC4/cafein/domain/Cafe.java
@@ -25,16 +25,25 @@ public class Cafe {
         this.longi = longi;
     }
 
-    public int getSeat() {
-        return seat;
+    public int getOccupied_s() {
+        return occupied_s;
     }
 
-    public void setSeat(int seat) {
-        this.seat = seat;
+    public void setOccupied_s(int seat) {
+        this.occupied_s = seat;
+    }
+
+    public int getTotal_s() {
+        return total_s;
+    }
+
+    public void setTotal_s(int total_s) {
+        this.total_s = total_s;
     }
 
     public String name;
     public String lati;
     public String longi;
-    public int seat;
+    public int occupied_s;
+    public int total_s;
 }
